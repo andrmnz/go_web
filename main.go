25fc5aa969affd854110777c5fa2c823ac48d059
andre <@aim.lc>
2022-05-29 00:39:41 +0200

    create and execute templates

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmpl, err := template.ParseFiles(filepath)
+
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	
+	err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("home"))
+	tmplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tmplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("contact"))
+	tmplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tmplPath)
 }
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("faq"))
+	tmplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tmplPath)
 }
 
 func main() {
