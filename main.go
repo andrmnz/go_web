22fc0d8f0418086ad672a7a7a4436013409bd48b
andre <@aim.lc>
2022-06-08 23:46:50 +0200

    using static handler in the controller package

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/andrmnz/go_web/controllers"
 	"github.com/andrmnz/go_web/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t, err := views.Parse(filepath)
-
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tmplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tmplPath)
-}
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tmplPath)
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	
+	tmpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+
+	if err != nil {
+		panic(err)
+	}
+	
+	r.Get("/", controllers.StaticHandler(tmpl))
+
+	tmpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+
+	if err != nil {
+		panic(err)
+	}
+	
+	r.Get("/contact", controllers.StaticHandler(tmpl))
+
+	tmpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/faq", controllers.StaticHandler(tmpl))
+	
+	
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	http.ListenAndServe(":8888", r)
 }
