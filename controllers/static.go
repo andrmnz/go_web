3537dd77ace5aae088a293e02a96d54800bfcb47
andre <iocry.eth@gmail.com>
2022-06-12 21:18:30 +0200

    dynamic templates

--- controllers/static.go
+++ controllers/static.go
@@ -1,13 +1,37 @@
 package controllers
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/andrmnz/go_web/views"
+	"github.com/andrmnz/go_web/views"
 )
 
 func StaticHandler(tmpl views.Template) http.HandlerFunc {
   return func(w http.ResponseWriter, r *http.Request) {
     tmpl.Execute(w, nil)
+  }
+}
+
+func FAQ(tmpl views.Template) http.HandlerFunc {
+  questions := []struct{
+    Question string
+    Answer string
+  }{
+    {
+    Question: "question uno",
+    Answer: "answer uno",
+    },
+    {
+    Question: "question dos",
+    Answer: "answer dos",
+    },
+    {
+    Question: "question tre",
+    Answer: "answer tre",
+    },
+  }
+  
+  return func (w http.ResponseWriter, r *http.Request) {
+    tmpl.Execute(w, questions)
   }
 }
