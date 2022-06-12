7a520aca58de695875a6bd496906374c1c22e247
andre <iocry.eth@gmail.com>
2022-06-12 21:54:30 +0200

    added tailwind css layout

--- main.go
+++ main.go
@@ -15,16 +15,25 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"layout.gohtml", "home.gohtml",
+	))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"layout.gohtml", "contact.gohtml",
+	))))
 
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"layout.gohtml", "faq.gohtml",
+	))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
-	
+
 	fmt.Println("Starting server on port 8888")
 
 	http.ListenAndServe(":8888", r)
